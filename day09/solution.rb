9402104c10e38f45f59ce6a04dc93740174d874d
anne <anne.thomas@mailbox.org>
2018-12-23 18:16:17 +0100

    Still stuck

--- day09/solution.rb
+++ day09/solution.rb
@@ -1,30 +1,36 @@
 require 'pry'
 
-def main
-  player_count = 13
+def play(p, v)
+  players = p
+  last_marble = v
   active_player = 0
-  last_marble = 7999
   active_marble = 0
   points = Hash.new(0)
   circle = [0]
   pointer = 0
 
-  while active_marble < last_marble
-    active_player = active_marble % player_count + 1
+  while active_marble <= last_marble
+    active_player = active_marble % players + 1
     active_marble += 1
-    puts active_player
     unless active_marble % 23 == 0
       pointer = (pointer + 2) % circle.length
       circle.insert(pointer, active_marble)
     else
-      # binding.pry
-      points[active_player] += active_marble
       pointer -= 7
-      points[active_player] += circle.slice!(pointer)
+      points[active_player] += active_marble + circle.slice!(pointer)
+
     end
   end
-  highscore = points.max_by { |_, val| val }
-  p highscore
+  points.max_by { |_, val| val }[1]
+end
+
+def main
+  puts play(9, 25) - 32
+  puts play(10, 1618) - 8317
+  puts play(13, 7999) - 146373
+  puts play(17, 1104) - 2764
+  puts play(21, 6111) - 54718
+  puts play(30, 5807) - 37305
 end
 
 main if $0 == __FILE__
