d60d3dbe706998034d942d110d27b8dcf7b5b2f5
anne <anne.thomas@mailbox.org>
2020-12-06 16:58:34 +0100

    Add day 5

---
+++ 2020/day05_02.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+passes = File.read('day05_input.txt').lines.map { |l| l.strip.split('') }
+
+max_id = 0
+
+seats = Array(1..848)
+
+passes.each do |pass|
+  row_instruction = pass.first(7)
+  col_instruction = pass.last(3)
+
+  rows = Array(0..127)
+  row_instruction.each do |i|
+    if i == 'F'
+      rows = rows.take(rows.length/2)
+    else
+      rows = rows.drop(rows.length/2)
+    end
+  end
+
+  cols = Array(0..7)
+  col_instruction.each do |i|
+    if i == 'L'
+      cols = cols.take(cols.length/2)
+    else
+      cols = cols.drop(cols.length/2)
+    end
+  end
+  id = rows.first * 8 + cols.first
+  seats.delete(id)
+end
+
+puts seats.inspect
+
+
+
